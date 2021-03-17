648d1f714022aff024a544316468faba0f56f8b2
Kael Moreno <kael.moreno@coreproc.ph>
2021-03-17 15:00:08 +0800

    Update dependencies, better configuration

--- app/src/main/java/com/coreproc/kotlin/kotlinbase/App.kt
+++ app/src/main/java/com/coreproc/kotlin/kotlinbase/App.kt
@@ -4,12 +4,13 @@ import com.coreproc.kotlin.kotlinbase.di.DaggerAppComponent
 import com.coreproc.kotlin.kotlinbase.utils.DeviceUtilities
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
+import io.sentry.android.core.SentryAndroid
 
-class App: DaggerApplication() {
+class App : DaggerApplication() {
 
     companion object {
         var instance: App? = null
-        fun getDeviceInfo() : DeviceUtilities? {
+        fun getDeviceInfo(): DeviceUtilities? {
             return instance!!.deviceUtil
         }
     }
@@ -20,11 +21,20 @@ class App: DaggerApplication() {
         return DaggerAppComponent.builder().application(this).build()
     }
 
-
     override fun onCreate() {
         super.onCreate()
 
         instance = this
         deviceUtil = DeviceUtilities(this)
+        initSentry()
+    }
+
+    private fun initSentry() {
+        if (BuildConfig.SENTRY_DSN.isNotEmpty()) {
+            SentryAndroid.init(this) {
+                it.dsn = BuildConfig.SENTRY_DSN
+                it.environment = BuildConfig.FLAVOR
+            }
+        }
     }
 }
