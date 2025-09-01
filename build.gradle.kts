88ea07692c68cf8d8e36c90be16c578b23ec2435
kaelmoreno-cp <kael.moreno@coreproc.com>
2025-09-01 18:14:03 +0800

    Refactor: Update Kotlin and dependency versions
    This commit updates various dependencies to their newer versions, including Kotlin and core libraries.
    
    Key changes:
    - Updated Kotlin version from `2.0.0` to `2.2.0` in `build.gradle.kts`.
    - Updated `com.google.dagger.hilt.android` plugin from `2.51.1` to `2.57.1`.
    - Updated `com.google.devtools.ksp` plugin from `2.0.0-1.0.24` to `2.2.0-2.0.2`.
    - Updated various library versions in `gradle/libs.versions.toml`:
        - `lifecycle` from `2.9.2` to `2.9.3`
        - `hilt` and `hiltCompiler` from `2.51.1` to `2.57.1`
        - `apache` from `3.7` to `3.18.0`
        - `retrofit` from `2.9.0` to `3.0.0`
        - `gson` from `2.11.0` to `2.13.1`
        - `okhttp` from `4.12.0` to `5.1.0`
        - `coroutines` from `1.9.0` to `1.10.2`
        - `kotlin` from `2.0.21` to `2.2.0`
        - `kotlinStdlib` from `2.1.20` to `2.2.0`
    - Removed `androidx-security-crypto` from `gradle/libs.versions.toml` as it was no longer referenced.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    val kotlinVersion by extra("2.0.0")
+    val kotlinVersion by extra("2.2.0")
     val gradleVersion by extra("8.10.1")
 
     repositories {
@@ -18,8 +18,8 @@ buildscript {
 }
 
 plugins {
-    id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.24" apply false
+    id("com.google.dagger.hilt.android") version "2.57.1" apply false
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
 }
 
 allprojects {
@@ -29,4 +29,3 @@ allprojects {
         maven(url = "https://plugins.gradle.org/m2/")
     }
 }
-
