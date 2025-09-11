209b3656dfc1edabdf193eb2152808e48cccfee3
kaelmoreno-cp <kael.moreno@coreproc.com>
2025-09-11 10:30:47 +0800

    Refactor: Update Theme to set status bar color based on current theme

--- app/src/main/java/com/coreproc/kotlin/kotlinbase/ui/theme/Theme.kt
+++ app/src/main/java/com/coreproc/kotlin/kotlinbase/ui/theme/Theme.kt
@@ -1,9 +1,14 @@
 package com.coreproc.kotlin.kotlinbase.ui.theme
 
+import android.app.Activity
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 // Light theme colors
 private val LightColorScheme = lightColorScheme(
@@ -63,6 +68,15 @@ fun KotlinBaseTheme(
     val colorScheme = when {
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+        }
     }
 
     MaterialTheme(
